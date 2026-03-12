4f7c77755e32f744f730da6d13ee171460bc9fe0
Sergio Graca <sergiofgraca@gmail.com>
2026-03-11 23:19:32 -0300

    fix home route

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('/login/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('', include('clientes.urls')),
 ]
